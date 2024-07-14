9cffc6da6cc6ec9422d3693b6a3642d8abba1a20
Emvanes <marcel@emvanes.nl>
2024-07-15 01:28:18 +0200

    Update Words_array_show_LCD_after_button_select_rev3.ino

--- Words_array_show_LCD_after_button_select_rev3/Words_array_show_LCD_after_button_select_rev3.ino
+++ Words_array_show_LCD_after_button_select_rev3/Words_array_show_LCD_after_button_select_rev3.ino
@@ -1,55 +1,64 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-const int toggleSwitchPin = 10;
+#define appName "RSM MIDI Controller"
+#define appVersion "v0.1"
 
-const int button1Pin = 7;
-const int button2Pin = 8;
+#define toggleSwitchPin 10
 
-const int serialBaud = 115200;
+#define button1Pin 7
+#define button2Pin 8
 
-const int buttonState_Off = 1;
-const int buttonState_On = 0;
-const int buttonState_Unknown = 3; //status knop weet je niet als MCU opstart; iemand kan de knop ingedrukt houden bij opstarten en dat geeft dus een verkeerde waarde als je er standaard vanuit gaat, dat hij normally open is
+#define serialBaud 115200
 
-const int lcd_Address = 0x27;
-const int lcd_NrCols = 20;
-const int lcd_NrLines = 2;
-const int lcd_FirstCol = 0;
-const int lcd_LastCol = lcd_FirstCol + lcd_NrCols - 1;
-const int lcd_FirstLine = 0;
-const int lcd_LastLine = lcd_FirstLine + lcd_NrLines - 1;
+#define lcd_Address 0x27
+#define lcd_NrCols 20
+#define lcd_NrLines 2
+#define lcd_FirstCol 0
+#define lcd_LastCol lcd_FirstCol + lcd_NrCols - 1
+#define lcd_FirstLine 0
+#define lcd_LastLine lcd_FirstLine + lcd_NrLines - 1
 
-const int midi_MinChan = 1;
-const int midi_MaxChan = 16;
-const int midi_NrChannels = 16;
+#define midi_NrChannels 16
+#define midi_MinChan 1
+#define midi_MaxChan midi_MinChan + midi_NrChannels -1
+
+#define buttonState_Off 1
+#define buttonState_On 0
+#define buttonState_Unknown 3 //status knop weet je niet als MCU opstart; iemand kan de knop ingedrukt houden bij opstarten en dat geeft dus een verkeerde waarde als je er standaard vanuit gaat, dat hij normally open is
 
 int prevtoggleSwitchState = buttonState_Unknown;
 int toggleSwitchState;
 
-int prevMillies;
-int curMillies;
+unsigned long prevMillies;
+unsigned long curMillies;
 
-int button1State = 0;
-int button2State = 0;
+uint8_t button1State = 0;
+uint8_t button2State = 0;
 
-int arrayIndex = 0;
+uint8_t arrayIndex = 0;
 
-String words[5] = {"EEN", "TWEE", "DRIE", "VIER", "VIJF"};
+String words[5] = {"EEN", "TWEE", "DRIE", "VIER", "VIJF"}; 
+
+LiquidCrystal_I2C lcd(lcd_Address, lcd_NrCols, lcd_NrLines);  
 
 void setup() {
+
+  String versionStr;
+
   pinMode(toggleSwitchPin, INPUT_PULLUP);
   pinMode(button1Pin, INPUT_PULLUP);
   pinMode(button2Pin, INPUT_PULLUP);
   Serial.begin(serialBaud);
-  LiquidCrystal_I2C lcd(lcd_Address, lcd_NrCols, lcd_NrLines);  
-  lcd.begin();
+//  LiquidCrystal_I2C lcd(lcd_Address, lcd_NrCols, lcd_NrLines);  
+  lcd.init();
   lcd.backlight();
   lcd.clear();
-  lcd.setCursor(lcd_FirstCol,lcd_FirstLine);
-  lcd.print("M coder was here");
-  lcd.setCursor(lcd_FirstCol,lcd_LastLine);
-  lcd.print("R was looking");
+  printToLCD(lcd_FirstCol,lcd_FirstLine, appName);
+//  printToLCD(lcd_FirstCol,lcd_LastLine,"version " + appVersion);
+  versionStr = "version ";
+  versionStr += appVersion;
+  printToLCD(lcd_FirstCol,lcd_LastLine,versionStr);  
   delay(100);
 }
 
@@ -72,11 +81,10 @@ void loop() {
 }
 
 void initToggleOnMode() {
-  lcd.clear();
   Serial.println("Toggle On mode");
+  lcd.clear();
   //delay(50);
-  lcd.setCursor(lcd_FirstCol, lcd_FirstLine);
-  lcd.print("toggleSwitch is ON");
+  printToLCD(lcd_FirstCol, lcd_FirstLine, "Toggle switch is ON ");
 }
 
 void handleToggleOnMode() {
@@ -84,10 +92,14 @@ void handleToggleOnMode() {
 }
 
 void initToggleOffMode() {
-  lcd.clear();
   Serial.println("Toggle Off mode");
-  lcd.setCursor(lcd_FirstCol, lcd_FirstLine);  
-  lcd.print("toggleSwitch is OFF");  
+  lcd.clear();
+  printToLCD(lcd_FirstCol, lcd_FirstLine,"Toggle switch is OFF");
+}
+
+void printToLCD(uint8_t col, uint8_t line, String text) {
+  lcd.setCursor(col, line);  
+  lcd.print(text);  
 }
 
 void handleToggleOffMode() {
@@ -99,9 +111,7 @@ void loop2() {
  do { 
   toggleSwitchState = digitalRead(toggleSwitchPin);
   if (toggleSwitchState != prevtoggleSwitchState){
-    lcd.setCursor(lcd_FirstCol, lcd_FirstLine);
-    lcd.print("toggleSwitch is OFF");
-
+    printToLCD(lcd_FirstCol, lcd_FirstLine, "Toggle switch is OFF");
   }
   prevtoggleSwitchState = toggleSwitchState;
   delay(50); }
@@ -114,12 +124,10 @@ lcd.clear();
 
 while(toggleSwitchState == LOW){
   
-  lcd.setCursor(lcd_FirstCol, lcd_FirstLine);
-  lcd.print("toggleSwitch is ON");
-  lcd.setCursor(lcd_FirstCol, lcd_LastLine);
-  lcd.print(words[arrayIndex]);  
-  lcd.setCursor(12,lcd_LastLine);
-  lcd.print(arrayIndex);
+  printToLCD(lcd_FirstCol, lcd_FirstLine, "Toggle switch is ON ");
+  printToLCD(lcd_FirstCol, lcd_LastLine, words[arrayIndex]);  
+
+  printToLCD(12, lcd_LastLine, arrayIndex);
   delay(1000);
   toggleSwitchState = digitalRead(toggleSwitchPin);
   button1State = digitalRead(button1Pin);
@@ -128,12 +136,9 @@ while(toggleSwitchState == LOW){
 
     if (arrayIndex >-1 && arrayIndex <= 3) {
       lcd.clear();
-      lcd.setCursor(lcd_FirstCol, lcd_FirstLine);
-      lcd.print("toggleSwitch is ON");
-      lcd.setCursor(lcd_FirstCol, lcd_LastLine);
-      lcd.print(words[arrayIndex++]);
-      lcd.setCursor(12,lcd_LastLine);
-      lcd.print(arrayIndex);
+      printToLCD(lcd_FirstCol, lcd_FirstLine, "toggleSwitch is ON ");
+      printToLCD(lcd_FirstCol, lcd_LastLine, words[arrayIndex++]);
+      printToLCD(12,lcd_LastLine, arrayIndex);
       //delay(500);
      }
     toggleSwitchState = digitalRead(toggleSwitchPin);
@@ -142,12 +147,9 @@ while(toggleSwitchState == LOW){
     if (arrayIndex >= 4) {
       arrayIndex = 0;
       lcd.clear();
-      lcd.setCursor(lcd_FirstCol, lcd_FirstLine);
-      lcd.print("toggleSwitch is ON");
-      lcd.setCursor(lcd_FirstCol, lcd_LastLine);
-      lcd.print(words[arrayIndex]);
-      lcd.setCursor(12,lcd_LastLine);
-      lcd.print(arrayIndex);
+      printToLCD(lcd_FirstCol, lcd_FirstLine, "Toggle switch is ON ");
+      printToLCD(lcd_FirstCol, lcd_LastLine, words[arrayIndex]);
+      printToLCP(12,lcd_LastLine, arrayIndex);
      }
    
   toggleSwitchState = digitalRead(toggleSwitchPin);
@@ -158,10 +160,8 @@ while(toggleSwitchState == LOW){
   if (toggleSwitchState == LOW && button2State == HIGH && arrayIndex >-1 || arrayIndex < 6) {
       lcd.clear();
       delay(50);
-      lcd.setCursor(lcd_FirstCol, lcd_FirstLine);
-      lcd.print("toggleSwitch is ON");
-      lcd.setCursor(lcd_FirstCol, lcd_LastLine);
-      lcd.print(words[arrayIndex--]);
+      printToLCD(lcd_FirstCol, lcd_FirstLine, "Toggle switch is ON ");
+      printToLCD(lcd_FirstCol, lcd_LastLine, words[arrayIndex--]);
       toggleSwitchState = digitalRead(toggleSwitchPin);
     }
 
@@ -169,10 +169,8 @@ while(toggleSwitchState == LOW){
       arrayIndex = 0;
       lcd.clear();
       delay(50);
-      lcd.setCursor(lcd_FirstCol, lcd_FirstLine);
-      lcd.print("toggleSwitch is ON");      
-      lcd.setCursor(lcd_FirstCol, lcd_LastLine);
-      lcd.print(words[arrayIndex]);
+      writeToLCD(lcd_FirstCol, lcd_FirstLine, "Toggle switch is ON ");
+      writeToLCD(lcd_FirstCol, lcd_LastLine, words[arrayIndex]);
       toggleSwitchState = digitalRead(toggleSwitchPin);
     }
 
@@ -180,18 +178,15 @@ while(toggleSwitchState == LOW){
       arrayIndex = 5;
       lcd.clear();
       delay(50);
-      lcd.setCursor(lcd_FirstCol, lcd_FirstLine);
-      lcd.print("toggleSwitch is ON");
-      lcd.setCursor(lcd_FirstCol, lcd_LastLine);
-      lcd.print(words[arrayIndex]);
+      writeToLCD(lcd_FirstCol, lcd_FirstLine, "toggle switch is ON ");
+      writeToLCD(lcd_FirstCol, lcd_LastLine, words[arrayIndex]);
       toggleSwitchState = digitalRead(toggleSwitchPin);
     }
  
   //toggleSwitchState = digitalRead(toggleSwitchPin);
 
   lcd.clear();
-  lcd.setCursor(lcd_FirstCol, lcd_LastLine);
-  lcd.print("terug start loop");
+  writeToLCD(lcd_FirstCol, lcd_LastLine, "terug start loop");
   delay(1000);
   lcd.clear();
   */
